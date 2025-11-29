129d3728db500d1def6853ca3f77f4e1b674e0b1
Cristian Riano <cristiancarm11@gmail.com>
2025-11-29 16:13:29 +0900

    Use <tab> for nvim autocomplete

---
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,21 @@
+return {
+  -- Source: https://github.com/LazyVim/LazyVim/discussions/39#discussioncomment-12370512
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+      },
+    },
+  }
+}
