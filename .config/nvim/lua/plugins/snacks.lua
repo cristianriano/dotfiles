1b40c8c881574895eb40658989574e1b39817889
Cristian Riano <cristiancarm11@gmail.com>
2025-12-13 14:39:00 +0900

    Nvim grep includes dotfiles

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,12 +1,13 @@
 return {
   "folke/snacks.nvim",
-  keys = {
-    {
-      "<leader>fe",
-      function()
-        Snacks.explorer({ cwd = LazyVim.root(), hidden = true })
-      end,
-      desc = "Explorer Snacks (root dir)",
+  opts = {
+    explorer = {
+      hidden = true,
+    },
+    picker = {
+      hidden = true,
     },
   },
+  -- Can override the keymap defined here https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/editor/snacks_explorer.lua#L8
+  keys = {},
 }
